1a5b00b5c8c81656f74638491eec6d2f95bc03a1
Junjie Mars <junjiemars@gmail.com>
2014-10-11 10:11:13 +0800

    add jvm options

--- project.clj
+++ project.clj
@@ -9,4 +9,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [redis.clients/jedis "2.6.0"]
                  [gnu.getopt/java-getopt "1.0.13"]]
+  :jvm-opts ["-Dline.separator=\n"]
   )
